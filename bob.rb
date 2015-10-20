34fe39a3d0d2ada493ff29c886ce105cdb231cf7
aquinofb <aquinofb@gmail.com>
2015-10-20 19:40:07 -0300

    Adding call method conforme Rack API specification

--- bob.rb
+++ bob.rb
@@ -12,6 +12,15 @@ module Bob
       route("GET", path, &handler)
     end
 
+    def call(env)
+      @request = Rack::Request.new(env)
+      verb = @request.request_method
+      requested_path = @request.path_info
+
+      handler = @routes[verb][requested_path]
+      handler.call
+    end
+
     private
       def route(verb, path, &handler)
         @routes[verb] ||= {}
