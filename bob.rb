1a2ce79180a56108b2761d44fb543e58476d983c
Felipe Aquino <aquinofb@gmail.com>
2015-10-21 18:58:25 -0300

    Delegate the route methods to Bob::Application

--- bob.rb
+++ bob.rb
@@ -57,4 +57,20 @@ module Bob
         @routes[verb][path] = handler
       end
   end
+
+  Application = Base.new
+
+  module Delegator
+    def self.delegate(*methods, opts={})
+      Array(methods).each do |method_name|
+        define_method(method_name) do |*args, &block|
+          opts[:to].send(method_name, *args, &block)
+        end
+
+        private method_name
+      end
+    end
+
+    delegate :get, :patch, :put, :post, :delete, :head, to: Application
+  end
 end
